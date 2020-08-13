02b2e38cab8232131cfc66929d058fe0f787e309
HughG-50 <hugh.greethead@gmail.com>
2020-08-13 16:41:02 +1000

    Name change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import React, { Component } from 'react';
 import PortfolioContainer from './PortfolioContainer';
 // import axios from 'axios';
 
-import './app.css';
+import './App.css';
 
 // const csrfToken = document.querySelector('[name="csrf-token"]').content;
 // axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
